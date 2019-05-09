463669ff8f7d0f908a5cae5957755f9480fb301c
Gautier <gautier19160@gmail.com>
2019-05-09 11:23:05 +0200

    Positionner nouveau vaisseau avec position donnée

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -3,14 +3,34 @@ package fr.unilim.iut.spaceinvaders;
 public class Vaisseau {
 	int x;
 	int y;
+	int longueur;
+	int hauteur;
 
-	public Vaisseau(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Vaisseau(int longueur, int hauteur) {
+		this(longueur, hauteur, 0, 0);
+	}
+	
+	public Vaisseau(int longueur, int largeur, int x, int y) {
+		this.longueur=longueur;
+		this.hauteur=largeur;
+		this.x=x;
+		this.y=y;
 	}
 
 	public boolean occupeLaPosition(int x, int y) {
-		return (this.x == x) && (this.y == y);
+	     return (estAbscisseCouverte(x) && estOrdonneeCouverte(y));
+    }
+
+	private boolean estOrdonneeCouverte(int y) {
+		return (this.y-this.hauteur+1<=y) && (y<=this.y);
+	}
+
+	private boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGAuche()<=x) && (x<=abscisseLaPlusADroite());
+	}
+
+	public int abscisseLaPlusADroite() {
+		return this.x+this.longueur-1;
 	}
 
 	public void seDeplacerVersLaDroite() {
@@ -21,11 +41,13 @@ public class Vaisseau {
 		this.x = this.x - 1;
 	}
 	
-	public int abscisse() {
+	public int abscisseLaPlusAGAuche() {
         return this.x;
 	}
 	
-	public int getX() {
-        return this.x;
-	}
+	public void positionner(int x, int y) {
+	    this.x = x;
+	    this.y = y;
+    }
+	
 }
